5b0d839ebfd2aa12888cd5c6e0043f788f8f4a2b
Noam Blumshtain <noamblu1@gmail.com>
2021-11-14 22:27:31 +0200

    fix code

--- strings.py
+++ strings.py
@@ -11,35 +11,32 @@ Example: ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
 """
 import pytest
 
-def no_duplicates(a_string):
-    pass
+def no_duplicates(a_string) -> str:
+    ls = list(set(a_string))
+    ls.sort()
+    return ''.join(ls)
 
+def reversed_words(a_string) -> str:
+    return a_string.split()[::-1]
 
-def reversed_words(a_string):
-    pass
+def four_char_strings(a_string) -> list:
+    return [a_string[i: i+4] for i in range(0, len(a_string), 4)]
 
-
-def four_char_strings(a_string):
-    pass
-
-
+# Tests Definition
 def test_no_duplicates():
     s = 'monty pythons flying circus'
     assert no_duplicates(s) == ' cfghilmnoprstuy'
-
 
 def test_reversed_words():
     s = 'monty pythons flying circus'
     assert reversed_words(s) == ['circus', 'flying', 'pythons', 'monty']
 
-
 def test_four_char_strings():
     s = 'monty pythons flying circus'
     assert four_char_strings(s) == ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
 
-
 def main():
-    return pytest.main(__file__)
+    return pytest.main([__file__])
 
 
 if __name__ == '__main__':
